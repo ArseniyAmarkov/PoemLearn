d038bf98ac0d3bc3d5fb9e0fe9e026f94c6f4384
ArseniyAmarkov <66740571+ArseniyAmarkov@users.noreply.github.com>
2020-10-04 16:47:33 +0300

    Add files via upload
    MainPage - файлы для работы главного окна, при создании приложения изменялись только они

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PoemLearn
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            author.Text = "Пушкин А. С.";
+            name.Text = "Керн";
+            poema.Text = "Я помню чудное мгновенье:\n" +
+                         "Передо мной явилась ты,\n" +
+                         "Как мимолетное виденье,\n" +
+                         "Как гений чистой красоты.";
+
+
+            // Прокрутка экрана на случай, если весь текст не помещается полностью в границы экрана
+            // Или в XAML через <ScrollView>в начале   </ScrollView>в конце
+            ScrollView scrollView = new ScrollView();
+            scrollView.Content = stackLabels;
+            this.Content = scrollView;
+        }
+    }
+}
