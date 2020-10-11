bb0a1b1ee409b6af66982d44a6fd9d1286d97178
ArseniyAmarkov <66740571+ArseniyAmarkov@users.noreply.github.com>
2020-10-12 00:18:35 +0300

    Update MainPage.xaml.cs
    Функция перехода на вторую страницу по нажатию кнопки

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -13,19 +13,14 @@ namespace PoemLearn
         public MainPage()
         {
             InitializeComponent();
-            author.Text = "Пушкин А. С.";
-            name.Text = "Керн";
-            poema.Text = "Я помню чудное мгновенье:\n" +
-                         "Передо мной явилась ты,\n" +
-                         "Как мимолетное виденье,\n" +
-                         "Как гений чистой красоты.";
 
 
-            // Прокрутка экрана на случай, если весь текст не помещается полностью в границы экрана
-            // Или в XAML через <ScrollView>в начале   </ScrollView>в конце
-            ScrollView scrollView = new ScrollView();
-            scrollView.Content = stackLabels;
-            this.Content = scrollView;
+        }
+
+        private async void SecondPageButton_Click(object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new SecondPage());
         }
     }
 }
+
